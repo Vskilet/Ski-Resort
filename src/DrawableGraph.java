6ce6e9532ae4a2ce9e0c3bf0d8d357ba6c76a535
Victor SENE <victor@sene.ovh>
2015-11-27 12:57:55 +0100

    Operationnel interface

--- src/DrawableGraph.java
+++ src/DrawableGraph.java
@@ -1,4 +1,4 @@
-import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.visualization.VisualizationImageServer;
 
@@ -6,42 +6,107 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 /**
  * Created by victor on 25/11/15.
  */
-public class DrawableGraph extends JFrame implements ActionListener{
+public class DrawableGraph extends JFrame implements ActionListener {
+    private Resort _lvl = new Resort();
     private Graph _graph;
     private Stack<Vertex> _shortestPath;
     private DirectedSparseMultigraph<Vertex, Edges> _directedGraph;
     private DirectedSparseMultigraph<Vertex, Edges> _directedShortestPath;
+    private ArrayList<Vertex> _dfs;
+
+    private JCheckBox chB;
+    private JCheckBox chV;
+    private JCheckBox chR;
+    private JCheckBox chN;
+    private JCheckBox chSURF;
+    private JCheckBox chKL;
+
     private JButton button;
+    private JButton b_dfs;
+    private JComboBox box1;
+    private JComboBox box2;
+    private JComboBox box_dfs;
+    private JLabel label1;
+    private VisualizationImageServer vs;
+    private VisualizationImageServer vs2;
+    //private BasicVisualizationServer<Integer,String> vv;
 
 
     public DrawableGraph(Graph graph, Stack<Vertex> shortestPath) throws HeadlessException {
         super("Graph");
+        this._lvl = new Resort();
         this._graph = graph;
         this._shortestPath = shortestPath;
         this._directedGraph = new DirectedSparseMultigraph<Vertex, Edges>();
         this._directedShortestPath = new DirectedSparseMultigraph<Vertex, Edges>();
-        this.button = new JButton("ERASE");
-        button.addActionListener(this);
+        this._dfs = new ArrayList<Vertex>();
 
-        this.setLayout(new GridLayout());
+        this.button = new JButton("Shortest Path");
+        button.addActionListener(this);
+        this.b_dfs = new JButton("DFS");
+        b_dfs.addActionListener(this);
+
+        this.chB = new JCheckBox("B");
+        chB.addActionListener(this);
+        this.chV = new JCheckBox("V");
+        chV.addActionListener(this);
+        this.chR = new JCheckBox("R");
+        chR.addActionListener(this);
+        this.chN = new JCheckBox("N");
+        chN.addActionListener(this);
+        this.chSURF = new JCheckBox("SURF");
+        chSURF.addActionListener(this);
+        this.chKL = new JCheckBox("KL");
+        chKL.addActionListener(this);
+
+        Object[] elements = new Object[]{"villaroger", "2", "3", "4", "aiguille-rouge", "6", "arc2000", "pre-saint-esprit", "9", "10", "11", "12", "grand-col", "14", "arpette", "16", "17", "18", "deux-tetes", "20", "21", "22", "col-des-frettes", "24", "25", "col-de-la-chal", "27", "28", "29", "arc1600", "31", "col-du-grand-renard", "33", "34", "35", "arc1800", "37"};
+        this.box1 = new JComboBox(elements);
+        this.box2 = new JComboBox(elements);
+        this.box_dfs = new JComboBox(elements);
+        this.label1 = new JLabel();
+        this.vs = new VisualizationImageServer(new FRLayout(this._directedGraph), new Dimension(450, 450));
+        this.vs2 = new VisualizationImageServer(new FRLayout(this._directedShortestPath), new Dimension(600, 600));
+
+        this.setLayout(new FlowLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocation(100,100);
+        this.setLocation(100, 100);
         this.setVisible(true);
     }
 
-    public void drawGraph(){
+    public void drawGraph() {
         this.conversionGraph();
         this.conversionShortPath();
         this.getContentPane().removeAll();
+
+        //this.getContentPane().add(vv);
+
+        this.getContentPane().add(chB);
+        this.getContentPane().add(chV);
+        this.getContentPane().add(chR);
+        this.getContentPane().add(chN);
+        this.getContentPane().add(chSURF);
+
+        this.getContentPane().add(box1);
+        this.getContentPane().add(box2);
         this.getContentPane().add(button);
 
-        VisualizationImageServer vs = new VisualizationImageServer(new CircleLayout(this._directedGraph), new Dimension(500, 500));
+        this.getContentPane().add(box_dfs);
+        this.getContentPane().add(b_dfs);
+
+        this.getContentPane().add(label1);
+
+        //VisualizationImageServer vs = new VisualizationImageServer(new FRLayout(this._directedGraph), new Dimension(500, 500));
+        //this.getContentPane().add(visualizationServer);
         this.getContentPane().add(vs);
+        //this.getContentPane().add(vs2);
+
+        //this.vs.repaint();
         this.pack();
     }
 
@@ -53,16 +118,107 @@ public class DrawableGraph extends JFrame implements ActionListener{
             _directedGraph.addEdge(e, e.get_fromVertex(), e.get_toVertex());
         }
     }
+
     public void conversionShortPath() {
-        for (Vertex v : _shortestPath){
+        for (Vertex v : _shortestPath) {
             _directedShortestPath.addVertex(v);
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        System.out.println("Bravo Geoffrey");
+        AbstractButton abstractButton = (AbstractButton) actionEvent.getSource();
+        boolean selected = abstractButton.getModel().isSelected();
 
-        drawGraph();
+        if (actionEvent.getActionCommand().equals("Shortest Path")) {
+            _shortestPath = _graph.shortestPath(this.box1.getModel().getSelectedItem().toString(), this.box2.getModel().getSelectedItem().toString(), _lvl);
+            this.conversionShortPath();
+            this.printShortestPath();
+            drawGraph();
+        } else if (actionEvent.getActionCommand().equals("DFS")) {
+            this._dfs = _graph.dfs(this.box_dfs.getModel().getSelectedItem().toString(), _lvl);
+            printDFS();
+        } else if (selected) {
+            if (actionEvent.getSource() == chB) {
+                _lvl.add(RoadType.B);
+                System.out.println("chB");
+            } else if (actionEvent.getSource() == chV) {
+                _lvl.add(RoadType.V);
+                System.out.println("chV");
+            } else if (actionEvent.getSource() == chR) {
+                _lvl.add(RoadType.R);
+                System.out.println("chR");
+            } else if (actionEvent.getSource() == chN) {
+                _lvl.add(RoadType.N);
+                System.out.println("chN");
+            } else if (actionEvent.getSource() == chSURF) {
+                _lvl.add(RoadType.SURF);
+                System.out.println("chSURF");
+            } else if (actionEvent.getSource() == chKL) {
+                _lvl.add(RoadType.KL);
+                System.out.println("chKL");
+            } else {
+                System.out.println("erreur");
+            }
+        } else if (!selected) {
+            if (actionEvent.getSource() == chB) {
+                _lvl.remove(RoadType.B);
+                System.out.println("uncheck B");
+            } else if (actionEvent.getSource() == chV) {
+                _lvl.remove(RoadType.V);
+                System.out.println("uncheck V");
+
+            } else if (actionEvent.getSource() == chR) {
+                _lvl.remove(RoadType.R);
+            } else if (actionEvent.getSource() == chN) {
+                _lvl.remove(RoadType.N);
+            } else if (actionEvent.getSource() == chSURF) {
+                _lvl.remove(RoadType.SURF);
+            } else if (actionEvent.getSource() == chKL) {
+                _lvl.remove(RoadType.KL);
+            } else {
+                System.out.println("else");
+            }
+        }
+    }
+
+    /*public void colorPath() {
+        final ArrayList<String> al = handleIntegerPath(_shortestPath); //simple fonction pour caster en string
+        Transformer<String, Paint> edgeStrokeTransformer2 = new Transformer<String, Paint>() {
+            public Paint transform(String sa) {
+                return (al.contains(sa)?Color.BLUE:Color.BLACK);
+            }
+        };
+        BasicVisualizationServer visualizationServer = null;
+        visualizationServer.getRenderContext().setEdgeDrawPaintTransformer(edgeStrokeTransformer2);
+        visualizationServer.repaint();
+    }
+    public ArrayList<String> handleIntegerPath(Stack<Vertex> arrayList){
+        ArrayList<String> al = new ArrayList<String>();
+        for (Vertex v : arrayList){
+            al.add(v.get_name().toString());
+        }
+        return  al;
+    }*/
+    public void printShortestPath() {
+        String out = "<html><body>ShortestPath<br/>";
+        System.out.println("ShortestPath");
+        for (Vertex v : _shortestPath) {
+            System.out.println(v);
+            out += v.toString() + "<br/>";
+        }
+        out += "</body></html>";
+        this.label1.setText(out);
+    }
+
+    public void printDFS() {
+        String out = "<html><body>DFS<br/>";
+        System.out.println("DFS");
+        for (Vertex v : _dfs) {
+            System.out.println(v);
+            out += v.toString() + "<br/>";
+        }
+        out += "</body></html>";
+        this.label1.setText(out);
     }
 }
